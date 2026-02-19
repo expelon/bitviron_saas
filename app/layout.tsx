e1bf0bf4b1fee005ee19e17a72fd8cdf1dcaf3ef
shabinvs <expelonofficial@gmail.com>
2026-02-19 12:34:00 +0530

    add google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Inter, Plus_Jakarta_Sans } from 'next/font/google';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import Script from "next/script";
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 const plusJakartaSans = Plus_Jakarta_Sans({
@@ -29,6 +30,21 @@ export default function RootLayout({
             <body
                 className={`${inter.variable} ${plusJakartaSans.variable} antialiased bg-white`}
             >
+                {/* Google Tag (gtag.js) */}
+                <Script
+                    async
+                    src="https://www.googletagmanager.com/gtag/js?id=G-BXPXP6GWYK"
+                    strategy="afterInteractive"
+                />
+                <Script id="google-analytics" strategy="afterInteractive">
+                    {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+
+                        gtag('config', 'G-BXPXP6GWYK');
+                    `}
+                </Script>
                 <Header />
                 {children}
                 <Footer />
